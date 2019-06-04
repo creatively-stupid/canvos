320adb5a6dda57f1d6a7c74c4e98d21044043044
RedMikePumpkin <michael@malinov.com>
2019-06-03 21:40:06 -0700

    did something

--- App/fs/apps/terminal/csh.js
+++ App/fs/apps/terminal/csh.js
@@ -1,4 +1,8 @@
 /*
  * this is the actual shell program, it runs on terminal start or when executing a .csc (Csh Shell Code) file,
- * it can also execute arbitrary .js files in "sudo" mode
+ * it can also execute arbitrary .js files
  */
+
+async function Run() {
+  
+}
