f1669b68ac62b393d979986ffb24f18442f32ce5
RedMikePumpkin <michael@malinov.com>
2019-08-23 13:13:43 +0000

    a

--- App/s.js
+++ App/s.js
@@ -419,7 +419,9 @@ function frame(time) {
           new Vector4(0, 0, 0, 1)
         )
       );
-    }, 0, size[1]/2, size[0]/2, size[0]/2);
+    }, 0, size[1]/2, size[0]/2, size[0]/2, [
+      images["unk.svg"]
+    ]);
     ctx.fillStyle = "#000";
     ctx.fillRect(size[0] * 2 * startMenuScroll, 0, size[0], 64);
     ctx.font = "400 30px seg";
@@ -2237,12 +2239,16 @@ class Camera3D {
 }
 
 class MeshTri {
-  constructor(p1, p2, p3, oc, fc) {
+  constructor(p1, p2, p3, oc, fc, uv1, uv2, uv3, txid) {
     this.p1 = p1 !== undefined ? p1 : new Vector3();
     this.p2 = p2 !== undefined ? p2 : new Vector3();
     this.p3 = p3 !== undefined ? p3 : new Vector3();
     this.oc = oc !== undefined ? oc : new Vector4(255, 255, 255, 255);
     this.fc = fc !== undefined ? fc : new Vector4(255, 0,   0,   255);
+    this.uv1 = uv1 !== undefined ? uv1 : new Vector2();
+    this.uv2 = uv2 !== undefined ? uv2 : new Vector2();
+    this.uv3 = uv3 !== undefined ? uv3 : new Vector2();
+    this.txid = txid !== undefined ? txid : 0;
   }
   tri() {
     return new Tri3D(
@@ -2257,7 +2263,7 @@ class Mesh {
   constructor(tris) {
     this.tris = tris !== undefined ? tris : [];
   }
-  render(ctx, convert, x, y, w, h) {
+  render(ctx, convert, x, y, w, h, textures) {
     var screentris = [];
     for (var i = 0, len = this.tris.length; i < len; i++) {
       ctx.strokeStyle = "rgba(" + this.tris[i].oc.x1 + ", " + this.tris[i].oc.x2 + ", " + this.tris[i].oc.x3 + ", " + this.tris[i].oc.x4 + ")";
@@ -2280,26 +2286,61 @@ var tritype = 2;
 // 1-classic
 // 2-torus
 
-function drawTriangle(c, tri) {
-  // c.beginPath();
-  // c.moveTo(tri.p1.x, tri.p1.y);
-  // c.lineTo(tri.p2.x, tri.p2.y);
-  // c.lineTo(tri.p3.x, tri.p3.y);
-  // c.lineTo(tri.p1.x, tri.p1.y);
-  // c.fill();
-  // c.stroke();
-  classic_drawTri(
-    Math.round(tri.p1.x/triscale), Math.round(tri.p1.y/triscale),
-    Math.round(tri.p2.x/triscale), Math.round(tri.p2.y/triscale),
-    Math.round(tri.p3.x/triscale), Math.round(tri.p3.y/triscale),
-    c
-  );
+function drawTriangle(c, tri, triuv) {
+  if (tritype === 0) {
+    c.beginPath();
+    c.moveTo(tri.p1.x, tri.p1.y);
+    c.lineTo(tri.p2.x, tri.p2.y);
+    c.lineTo(tri.p3.x, tri.p3.y);
+    c.lineTo(tri.p1.x, tri.p1.y);
+    c.fill();
+    c.stroke();
+  } else if (tritype === 1) {
+    classic_drawTri(
+      Math.round(tri.p1.x/triscale), Math.round(tri.p1.y/triscale),
+      Math.round(tri.p2.x/triscale), Math.round(tri.p2.y/triscale),
+      Math.round(tri.p3.x/triscale), Math.round(tri.p3.y/triscale),
+      c
+    );
+  } else if (tritype === 2) {
+
+  }
 }
 
 function classichelper_hline(x, y, l, ctx) {
   for (var i = 0; i < l; i++) {
     ctx.fillRect(Math.round(x + i)*triscale, Math.round(y)*triscale, triscale, triscale);
   }
+}
+
+function textureMap(ctx, texture, pts) {
+  var x0 = pts[0].x, x1 = pts[1].x, x2 = pts[2].x;
+  var y0 = pts[0].y, y1 = pts[1].y, y2 = pts[2].y;
+  var u0 = pts[0].u, u1 = pts[1].u, u2 = pts[2].u;
+  var v0 = pts[0].v, v1 = pts[1].v, v2 = pts[2].v;
+
+  // Set clipping area so that only pixels inside the triangle will
+  // be affected by the image drawing operation
+  ctx.save(); ctx.beginPath(); ctx.moveTo(x0, y0); ctx.lineTo(x1, y1);
+  ctx.lineTo(x2, y2); ctx.closePath(); ctx.clip();
+
+  // Compute matrix transform
+  var delta = u0*v1 + v0*u2 + u1*v2 - v1*u2 - v0*u1 - u0*v2;
+  var delta_a = x0*v1 + v0*x2 + x1*v2 - v1*x2 - v0*x1 - x0*v2;
+  var delta_b = u0*x1 + x0*u2 + u1*x2 - x1*u2 - x0*u1 - u0*x2;
+  var delta_c = u0*v1*x2 + v0*x1*u2 + x0*u1*v2 - x0*v1*u2
+                - v0*u1*x2 - u0*x1*v2;
+  var delta_d = y0*v1 + v0*y2 + y1*v2 - v1*y2 - v0*y1 - y0*v2;
+  var delta_e = u0*y1 + y0*u2 + u1*y2 - y1*u2 - y0*u1 - u0*y2;
+  var delta_f = u0*v1*y2 + v0*y1*u2 + y0*u1*v2 - y0*v1*u2
+                - v0*u1*y2 - u0*y1*v2;
+
+  // Draw the transformed image
+  ctx.transform(delta_a/delta, delta_d/delta,
+                delta_b/delta, delta_e/delta,
+                delta_c/delta, delta_f/delta);
+  ctx.drawImage(texture, 0, 0);
+  ctx.restore();
 }
 
 function classic_drawTri(x0, y0, x1, y1, x2, y2, ctx) {
@@ -2385,14 +2426,22 @@ var cubeMesh = new Mesh([
     new Vector3( 0.5,  0.5, -0.5),
     new Vector3( 0.5, -0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 0),
+    new Vector2(1, 0),
+    new Vector2(1, 1),
+    0
   ),
   new MeshTri(
     new Vector3(-0.5, -0.5, -0.5),
     new Vector3(-0.5,  0.5, -0.5),
     new Vector3( 0.5, -0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 1),
+    new Vector2(0, 0),
+    new Vector2(1, 1),
+    0
   ),
   // back
   new MeshTri(
@@ -2400,14 +2449,22 @@ var cubeMesh = new Mesh([
     new Vector3(-0.5,  0.5,  0.5),
     new Vector3( 0.5, -0.5,  0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(1, 0),
+    new Vector2(0, 0),
+    new Vector2(1, 1),
+    0
   ),
   new MeshTri(
     new Vector3(-0.5,  0.5,  0.5),
     new Vector3(-0.5, -0.5,  0.5),
     new Vector3( 0.5, -0.5,  0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 0),
+    new Vector2(0, 1),
+    new Vector2(1, 1),
+    0
   ),
   // top
   new MeshTri(
@@ -2415,14 +2472,22 @@ var cubeMesh = new Mesh([
     new Vector3( 0.5,  0.5,  0.5),
     new Vector3( 0.5,  0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 1),
+    new Vector2(1, 1),
+    new Vector2(1, 0),
+    0
   ),
   new MeshTri(
     new Vector3(-0.5,  0.5, -0.5),
     new Vector3(-0.5,  0.5,  0.5),
     new Vector3( 0.5,  0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 0),
+    new Vector2(0, 1),
+    new Vector2(1, 0),
+    0
   ),
   // bottom
   new MeshTri(
@@ -2430,14 +2495,22 @@ var cubeMesh = new Mesh([
     new Vector3(-0.5, -0.5,  0.5),
     new Vector3( 0.5, -0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(0, 0),
+    new Vector2(1, 0),
+    new Vector2(0, 1),
+    0
   ),
   new MeshTri(
     new Vector3(-0.5, -0.5,  0.5),
     new Vector3(-0.5, -0.5, -0.5),
     new Vector3( 0.5, -0.5, -0.5),
     new Vector4(0,   0, 255, 255),
-    new Vector4(0,   0, 255, 255)
+    new Vector4(0,   0, 255, 255),
+    new Vector2(1, 0),
+    new Vector2(1, 1),
+    new Vector2(0, 1),
+    0
   ),
   // left
   new MeshTri(
@@ -2445,14 +2518,22 @@ var cubeMesh = new Mesh([
     new Vector3(-0.5,  0.5, -0.5),
     new Vector3(-0.5, -0.5,  0.5),
     new Vector4(0,  64, 192, 255),
-    new Vector4(0,  64, 192, 255)
+    new Vector4(0,  64, 192, 255),
+    new Vector2(0, 0),
+    new Vector2(1, 0),
+    new Vector2(0, 1),
+    0
   ),
   new MeshTri(
     new Vector3(-0.5, -0.5, -0.5),
     new Vector3(-0.5, -0.5,  0.5),
     new Vector3(-0.5,  0.5, -0.5),
     new Vector4(0,  64, 192, 255),
-    new Vector4(0,  64, 192, 255)
+    new Vector4(0,  64, 192, 255),
+    new Vector2(1, 1),
+    new Vector2(0, 1),
+    new Vector2(1, 0),
+    0
   ),
   // right
   new MeshTri(
@@ -2460,13 +2541,21 @@ var cubeMesh = new Mesh([
     new Vector3( 0.5,  0.5,  0.5),
     new Vector3( 0.5, -0.5,  0.5),
     new Vector4(0,  64, 192, 255),
-    new Vector4(0,  64, 192, 255)
+    new Vector4(0,  64, 192, 255),
+    new Vector2(0, 1),
+    new Vector2(1, 0),
+    new Vector2(1, 0),
+    0
   ),
   new MeshTri(
     new Vector3( 0.5, -0.5,  0.5),
     new Vector3( 0.5, -0.5, -0.5),
     new Vector3( 0.5,  0.5, -0.5),
     new Vector4(0,  64, 192, 255),
-    new Vector4(0,  64, 192, 255)
+    new Vector4(0,  64, 192, 255),
+    new Vector2(1, 0),
+    new Vector2(1, 1),
+    new Vector2(0, 1),
+    0
   ),
 ]);
